f222bd1fc16df46b79b8b74893e736c10ea5fb75
Ranjan Kishor <ranjankishor83@gmail.com>
2023-08-23 12:47:25 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState<any>("");
+  const [password, setPassword] = useState<any>("");
+  const [user, setUser] = useState<any>([]);
+
+  const handleSubmit = () => {
+    let newuser = {
+      email: email,
+      password: password,
+    };
+    setUser([...user, newuser]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <br></br>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button onClick={handleSubmit}>kmk</button>
+
+      {user.map((x: any) => {
+        return <h1>{x.email}</h1>;
+      })}
     </div>
   );
 }
